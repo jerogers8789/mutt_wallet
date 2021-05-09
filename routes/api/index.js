00d842de6d8bf39962da50ad4b8334fc6e5184c1
JER.8789 <je.rogers8789@gmail.com>
2021-05-09 18:05:19 -0500

    edited user route, was missing the catch for try/catch

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,15 +1,8 @@
 const router = require('express').Router();
-<<<<<<< HEAD
 const userRoutes = require('./user-routes');
 const acctRoutes = require('./wallet-routes');
 
 router.use('/user', userRoutes);
 router.use('/wallet', acctRoutes);
-=======
-const userRoute = require('./user-route');
-const walletRoute = require('./wallet-route');
-router.use('/wallet', walletRoute);
-router.use('/users', userRoute);
->>>>>>> 74276a730d6669cdd1c5fc8e889e2da9a7c7afdc
 
 module.exports = router;
