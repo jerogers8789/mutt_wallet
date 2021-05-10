aab92e1c642e2663d18e7fcbc729224d670e7156
josephmartin721 <josephmartin721@yahoo.com>
2021-05-10 08:39:30 -0500

    accepted incoming changes to local documentation

--- seeds/wallet-seeds.js
+++ seeds/wallet-seeds.js
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-const { Post } = require('../models');
-
-const postData = [
-    {
-        title: 'wallet 1',
-        post_content: 'placeholder text values',
-    },
-    {
-        title: 'wallet 2',
-        post_content: 'placeholder text values',
-    },
-]
-
-const seedPosts = () => Post.bulkCreate(postData);
-
-module.exports = seedPosts;
-=======
 const { Wallet } = require('../models');
 
 const walletData = [
@@ -32,4 +14,3 @@ const seedWallet = () => Wallet.bulkCreate(walletData);
 
 module.exports = seedWallet;
 
->>>>>>> 74276a730d6669cdd1c5fc8e889e2da9a7c7afdc
