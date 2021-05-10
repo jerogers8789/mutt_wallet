53ff7be5cd6a99825e2555faf5222efed92ce0a1
josephmartin721 <josephmartin721@yahoo.com>
2021-05-09 21:30:14 -0500

    stylystic changes in handlebars - server style changes

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const path = require("path")
-const express = require("express")
-const express_handlebars = require("express-handlebars")
-const sequelize = require("./config/connection.js")
-const { dirname } = require("path")
-const { Server } = require("http")
-const app = express()
-const PORT = process.env.PORT||3001
-const hbs = express_handlebars.create()
+const path = require("path");
+const express = require("express");
+const express_handlebars = require("express-handlebars");
+const sequelize = require("./config/connection.js");
+const { dirname } = require("path");
+const { Server } = require("http");
+const app = express();
+const PORT = process.env.PORT||3001;
+const hbs = express_handlebars.create();
 
-app.engine("handlebars",hbs.engine)
+app.engine("handlebars",hbs.engine);
 
-app.set("view engine", "handlebars")
+app.set("view engine", "handlebars");
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname,"public")));
 
 app.get("/", (req, res)=>{
     res.render("homepage")
-})
+});
 app.get("/", (req, res)=>{
     res.render("muttcoin")
-})
+});
 
-app.listen(PORT, function(){console.log("portListening")})
+app.listen(PORT, function(){console.log("portListening")});
