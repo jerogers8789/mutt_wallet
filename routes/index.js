b158f4f1c537579db125144582a9bbbe3f15307b
josephmartin721 <josephmartin721@yahoo.com>
2021-05-09 22:48:48 -0500

    merge changes I had missed

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,5 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-<<<<<<< HEAD
-
-router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
-});
-
-=======
 const homepageRoute = require('./homepage-route.js');
 const dashRoute = require('./dash-route.js');
 router.use('/api', apiRoutes);
@@ -17,5 +8,4 @@ router.use('/dashboard', dashRoute);
 router.use((req, res) => {
     res.status(404).end();
 });
->>>>>>> 74276a730d6669cdd1c5fc8e889e2da9a7c7afdc
 module.exports = router;
