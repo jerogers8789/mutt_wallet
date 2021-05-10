e0398cce29afe90ac547d7580def44750f239669
josephmartin721 <josephmartin721@yahoo.com>
2021-05-10 16:09:20 -0500

    reverting back to previous server details

--- server.js
+++ server.js
@@ -1,38 +1,26 @@
+const path = require("path")
+const express = require("express")
+const express_handlebars = require("express-handlebars")
+const sequelize = require("./config/connection.js")
+const { dirname } = require("path")
+const { Server } = require("http")
+const app = express()
+const PORT = process.env.PORT||3001
+const hbs = express_handlebars.create()
 
-const path = require("path");
-const express_handlebars = require("express-handlebars");
-const sequelize = require("./config/connection.js");
-const { dirname } = require("path");
-const { Server } = require("http");
-const hbs = express_handlebars.create();
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');;
-const routes = require('./seeds');
+app.engine("handlebars",hbs.engine)
 
-// import sequelize connection
+app.set("view engine", "handlebars")
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(routes);
-
-// sync sequelize models to the database, then turn on the server
-sequelize.sync({force: false}).then(() => {
-
-app.engine("handlebars",hbs.engine);
-
-app.set("view engine", "handlebars");
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname,"public")))
 
 app.get("/", (req, res)=>{
-    res.render("muttcoin")
-});
-
-app.listen(PORT, function(){console.log("portListening")
+    res.render("homepage")
 })
-}
-);
+app.get("/", (req, res)=>{
+    res.render("muttcoin")
+})
+
+app.listen(PORT, function(){console.log("portListening")})
