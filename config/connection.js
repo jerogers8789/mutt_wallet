b8bf7470a9ff012d4afa3b7ec5746344c4006541
josephmartin721 <josephmartin721@yahoo.com>
2021-05-09 20:19:05 -0500

    accepted incoming changes

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
@@ -13,20 +12,4 @@ const sequelize = process.env.JAWSDB_URL
       },
     });
 
-=======
-require('dotenv').config();
-
-const Sequelize = require('sequelize');
-
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
-
->>>>>>> 74276a730d6669cdd1c5fc8e889e2da9a7c7afdc
 module.exports = sequelize;
